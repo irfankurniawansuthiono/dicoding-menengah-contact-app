ff8f3c84b5e931257d0cf65c9007f51d8eb08c7b
Irfan Kurniawan <kurniawanmarc93@gmail.com>
2023-12-14 23:02:30 +0700

    Delete src directory

--- src/hooks/PrivateRoute.jsx
+++
@@ -1,6 +0,0 @@
-import { getAccessToken } from "../api/api";
-import { Navigate } from "react-router-dom";
-export default function PrivateRoute({ components }) {
-  const token = getAccessToken();
-  return token ? components : <Navigate to={"/login"} />;
-}
